6d46f9f797af9bf69437662a5e0abefa102096eb
Baejita <homokoro@gmail.com>
2024-09-29 17:55:16 +0700

    ok delete User

--- app/api/totalusers/route.js
+++ app/api/totalusers/route.js
@@ -7,3 +7,12 @@ export async function GET() {
   const totalUsers = await User.find();
   return NextResponse.json({ totalUsers });
 }
+
+export async function DELETE(req) {
+  await connectMongoDB();
+  const userId = req.nextUrl.searchParams.get("id");
+  const user = await User.findByIdAndDelete(userId);
+  return NextResponse.json({ message: "User Deleted" }, user, {
+    status: "deleted",
+  });
+}
